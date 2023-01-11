fc1d0f878ce9eba4d2b1e2d11e6bd0aaaeb3efb2
tempesthyq <87917846+tempesthyq@users.noreply.github.com>
2023-01-11 10:34:57 +0800

    feat:一个比较拉的移动系统

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -19,10 +19,16 @@ public class CameraFollow : MonoBehaviour
             if(transform.position != target.position)
             {
                 Vector3 targetPos = target.position;
+                targetPos.x = Mathf.Clamp(targetPos.x, minPosition.x, maxPosition.x);
+                targetPos.y = Mathf.Clamp(targetPos.y, minPosition.y, maxPosition.y);
                 transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
             }
         }
     }
 
-
+    public void SetCamPosLimit(Vector2 minPos, Vector2 maxPos)
+    {
+        minPosition = minPos;
+        maxPosition = maxPos;
+    }
 }
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D rb;
+    public Collider2D coll;
+    public Animation anim;
+
+    public float jumpForce,speed;
+    public Transform groundCheck;
+    public LayerMask ground;
+
+    public bool isGround, isJump;
+     
+    bool jumpPressed;
+    public int jumpCount;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&&jumpCount>0)
+        {
+            jumpPressed = true;
+            Debug.Log("aaa");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, ground);
+        GroundMove();
+        Jump();
+    }
+
+    void GroundMove()
+    {
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
+
+        if (horizontalMove != 0)
+        {
+            transform.localScale = new Vector3(horizontalMove, 1, 1);
+        }
+    }
+
+    void Jump()
+    {
+        Debug.Log(isGround+"isGround");
+        Debug.Log(jumpPressed + "jumpPressed");
+
+        if (isGround)
+        {
+            jumpCount = 2;
+            isJump = false;
+        }
+        if (isGround && jumpPressed)
+        {
+            Debug.Log("ttt");
+            isJump = true;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--;
+            jumpPressed = false;
+        }
+        else if (jumpPressed && jumpCount > 0 && isJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--;
+            jumpPressed = false;
+        }
+    }
+}
