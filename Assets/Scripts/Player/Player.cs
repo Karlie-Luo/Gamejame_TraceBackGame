31e7e26c9358255e26e5466cf603f389acb71ace
tempesthyq <87917846+tempesthyq@users.noreply.github.com>
2023-01-15 14:42:35 +0800

    fix:不知道都修了什么东西

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -78,6 +78,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(isGround);
         if(playerDeath)
         {
             Debug.Log("doblink");
@@ -176,6 +177,7 @@ public class Player : MonoBehaviour
         if (isGround && jumpPressed)
         {
             isJump = true;
+            isGround = false;
             cannotDrag = true;
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             jumpCount--;
@@ -250,7 +252,7 @@ public class Player : MonoBehaviour
     private void ColliderTest(bool Ground,Collider2D collision)
     {
         Vector2 v = collision.ClosestPoint(this.transform.position);
-        if (v.y < transform.position.y && Mathf.Abs(v.x - transform.position.x) < 0.1)
+        if (v.y < transform.position.y+2 && Mathf.Abs(v.x - transform.position.x) < 1)
         {
             isGround = Ground;
             animt.SetBool("isGround", Ground);
